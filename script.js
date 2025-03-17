27448b0124769a7f23bb6c32c1fa08051f689b51
M1N00 <antoni.debaz@gmail.com>
2025-03-17 18:31:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,36 +6,44 @@ firebase.initializeApp({
     storageBucket: "nonamemarcobdd.firebasestorage.app",
     messagingSenderId: "544836475440",
     appId: "1:544836475440:web:6632eff99dcfbeb1e7abf5",
-
 });
 
 // Récupérer une instance de Firebase Storage
 var storage = firebase.storage();
 
-// Créer une référence vers le répertoire contenant les images
+// Créer une référence vers le répertoire contenant les fichiers
 var storageRef = storage.ref();
 
-// Lister tous les fichiers qui commencent par "img"
+// Lister tous les fichiers
 storageRef.listAll()
   .then(function(result) {
-    result.items.forEach(function(imageRef) {
-      // Vérifie si le fichier commence par "img"
-      if (imageRef.name.startsWith('img')) {
-        // Récupérer l'URL de téléchargement du fichier
-        imageRef.getDownloadURL()
-          .then(function(url) {
-            // Créer une balise <img> et ajouter l'URL comme source
-            var imgElement = document.createElement('img');
-            imgElement.src = url;
-            
-            // Ajouter l'image au conteneur
-            document.getElementById('container').appendChild(imgElement);
-          })
-          .catch(function(error) {
-            // Gestion des erreurs (fichier non trouvé, permissions insuffisantes, etc.)
-            console.error("Erreur lors de la récupération du fichier :", error);
-          });
-      }
+    result.items.forEach(function(fileRef) {
+      // Récupérer l'URL de téléchargement du fichier
+      fileRef.getDownloadURL()
+        .then(function(url) {
+          // Créer une balise <img> ou <video> selon le type de fichier
+          var fileType = fileRef.name.split('.').pop().toLowerCase();
+          var mediaElement;
+          
+          if (['jpg', 'jpeg', 'png', 'gif'].includes(fileType)) {
+            mediaElement = document.createElement('img');
+            mediaElement.src = url;
+          } else if (['mp4', 'webm', 'ogg'].includes(fileType)) {
+            mediaElement = document.createElement('video');
+            mediaElement.src = url;
+            mediaElement.controls = true;
+            mediaElement.preload = 'metadata';
+          }
+          
+          // Ajouter le média au conteneur
+          if (mediaElement) {
+            document.getElementById('container').appendChild(mediaElement);
+          }
+        })
+        .catch(function(error) {
+          // Gestion des erreurs (fichier non trouvé, permissions insuffisantes, etc.)
+          console.error("Erreur lors de la récupération du fichier :", error);
+        });
     });
   })
   .catch(function(error) {
